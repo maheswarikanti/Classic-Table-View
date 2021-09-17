fa9baad770b6d5a5e3e64778f62585bc5938bca3
Maheswari Kanti <kanti.m@northeastern.edu>
2021-09-16 20:11:57 -0700

    Initial Commit

---
+++ Classic Table View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Classic Table View
+//
+//  Created by Maheswari Kanti on 9/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
